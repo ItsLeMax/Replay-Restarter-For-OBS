b53b29391be639cba758bda7effbce9dd4b00804
ItsLeMax <lemaxyt1243@gmail.com>
2024-11-29 17:54:48 +0100

    time option added, description and logs updated, QoL

--- OBS-Buffer-Clear.lua
+++ OBS-Buffer-Clear.lua
@@ -1,16 +1,22 @@
 obs = obslua
 
 local is_enabled = true
+local time_until_clear = 1.50
 
 function script_description()
     return "Restarts the replay buffer when saving a clip to prevent clip overlapping\n\n"..
-    "Take into consideration, that this script requires a delay of one second between each toggle where nothing is recorded."
+    "Recommended timing based on own testing:\n\n"..
+    "This script requires a delay of at least half a second between each toggle where nothing is recorded. "..
+    "The values below may depend on your computers hardware- and or software.\n"..
+    "Risky: 0.50s | Fast: 1.00s | Default: 1.50s"
 end
 
 function script_properties()
     local props = obs.obs_properties_create()
 
     obs.obs_properties_add_bool(props, "enable_script", "Enable script functionality")
+
+    obs.obs_properties_add_float(props, "interval_seconds", "Time until clear", .5, 30, .25)
 
     obs.obs_properties_add_button(props, "restart_button", "Simulate Replay save", function()
         restart_replay_buffer()
@@ -21,6 +27,7 @@ end
 
 function script_defaults(settings)
     obs.obs_data_set_default_bool(settings, "enable_script", true)
+    obs.obs_data_set_default_double(settings, "interval_seconds", time_until_clear)
 end
 
 function script_load(settings)
@@ -33,6 +40,7 @@ end
 
 function script_update(settings)
     is_enabled = obs.obs_data_get_bool(settings, "enable_script")
+    time_until_clear = obs.obs_data_get_double(settings, "interval_seconds")
 end
 
 function restart_replay_buffer()
@@ -41,14 +49,14 @@ function restart_replay_buffer()
     end
 
     if obs.obs_frontend_replay_buffer_active() then
-        obs.script_log(obs.LOG_INFO, "Restarting Replay Buffer...")
+        obs.script_log(obs.LOG_INFO, string.format("Restarting Replay Buffer (Duration: %.2f sec)", time_until_clear))
         obs.obs_frontend_replay_buffer_stop()
         obs.timer_add(function()
             obs.obs_frontend_replay_buffer_start()
-            obs.script_log(obs.LOG_INFO, "Replay Buffer restarted successfully.")
+            obs.script_log(obs.LOG_INFO, "Replay Buffer restarted successfully")
             obs.remove_current_callback()
-        end, 1000)
+        end, time_until_clear * 1000)
     else
-        obs.script_log(obs.LOG_WARNING, "Replay Buffer is not active. Make sure it is for the script to work.")
+        obs.script_log(obs.LOG_WARNING, "Replay Buffer has to be active for simulation")
     end
 end
